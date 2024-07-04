c1afa49567231d612469da7562b9a4f7f22b020b
Saheed Ademola Adewale <136191002+Hardeymorlah12@users.noreply.github.com>
2024-07-04 08:04:50 +0100

    Add files via upload

---
+++ AbbeyFullStackApp/src/main/java/Hardeymorlah/AbbeyFullStackApp/AbbeyFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package Hardeymorlah.AbbeyFullStackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbeyFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbeyFullStackAppApplication.class, args);
+	}
+
+}
---
+++ AbbeyFullStackApp/src/test/java/Hardeymorlah/AbbeyFullStackApp/AbbeyFullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Hardeymorlah.AbbeyFullStackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbeyFullStackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
